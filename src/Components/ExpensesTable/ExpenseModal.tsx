3e86f1cdb0a7f74ed62669f058154315f7f1fe17
Gulshan Ahmad <gulshanahmad68@gmail.com>
2025-03-04 12:40:30 +0500

    change Background color

--- src/Components/ExpensesTable/ExpenseModal.tsx
+++ src/Components/ExpensesTable/ExpenseModal.tsx
@@ -3,8 +3,6 @@ import styled from "styled-components";
 
 const ModalBackground = styled.div`
   position: fixed;
-  top: 0;
-  left: 0;
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.5);
